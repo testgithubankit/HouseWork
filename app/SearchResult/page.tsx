9a8cfcf850ca9843554d896c1fbb1953c04b550f
testgithubankit <140956234+testgithubankit@users.noreply.github.com>
2024-01-31 21:55:47 +0530

    update

--- app/SearchResult/page.tsx
+++ app/SearchResult/page.tsx
@@ -1,18 +1,15 @@
 "use client"
 import React from 'react';
 import DoctorSearch from './Doctor/DoctorSearch';
-// import SubmitCaseForReview from '@/Components/SubmitCaseForReview';
+
 
 
 
 const SearchDoctors = () => {
   return (
     <>
-      <section
-        className="SearchDoctors mb-1"
-      >
+      <section className="SearchDoctors mb-1">
         <DoctorSearch />
-        {/* <SubmitCaseForReview /> */}
       </section>
     </>
   );
